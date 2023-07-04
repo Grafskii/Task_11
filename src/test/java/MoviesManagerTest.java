d021608492f6f1b4f34e0e5e491b1fa7f0a4302a
Grafskii <miti-h@yandex.ru>
2023-07-04 13:42:21 +0300

    Two commit

--- src/test/java/MoviesManagerTest.java
+++ src/test/java/MoviesManagerTest.java
@@ -52,4 +52,21 @@ public class MoviesManagerTest {
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void limitAboveMaximum() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5", "Film 6", "Film 7"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
